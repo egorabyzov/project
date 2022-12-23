4c4255fd76c9ec8646f372713cf275e99f2439f7
egorabyzov <byzov_E@mail.ru>
2022-12-23 11:20:54 +0300

    doneChtotoTam

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -2,40 +2,36 @@ import React, { useEffect, useState } from 'react';
 import { redirect } from 'react-router-dom';
 import Page from './Page';
 
-// условно нам это приходит из БД
-
 export default function MainPage({ user, activity }) {
   const [init, setInit] = useState([]);
-  const [searchLevel, setSearchLevel] = useState({ razdel: '', level: '' });
   const [fo, setFo] = useState({});
-  // console.log(activity);
-  const [cards, setCards] = useState(activity);
+  const [initFilter, setInitFilter] = useState([]);
+  const [isSearchLevel, setIsSearchLevel] = useState('');
+  const [isSearchCategory, setIsSearchCategory] = useState('');
 
-  console.log(searchLevel, 'searchLevel');
+  const changeHandlerLevel = (e) => {
+    setIsSearchLevel(!!e.target.value);
+    setInitFilter([...init].filter((el) => el.Level.id === +e.target.value));
+  };
+  const changeHandlerCategory = (e) => {
+    setIsSearchCategory(!!e.target.value);
+    setInitFilter([...init].filter((el) => el.Category.id === +e.target.value));
+  };
+
   useEffect(() => {
     fetch('api/initall')
       .then((res) => res.json())
-      .then((data) => setInit(data));
+      .then((data) => {
+        setInitFilter(data);
+        setInit(data);
+      });
   }, []);
 
-  // useEffect(() => {
-  //   fetch('api/initall')
-  //     .then((res) => res.json())
-  //     .then((data) => setInit((data.filter((el) => el.Level.name === (searchLevel?.razdel || searchLevel?.level))) || data));
-  // }, [searchLevel]);
-
   useEffect(() => {
-    console.log('YA TUT');
     fetch('/auth/allvalue')
       .then((res) => res.json())
       .then((data) => setFo(data));
   }, []);
-
-  const changeHandler = (e) => {
-    setSearchLevel((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-  };
-  console.log(init);
-  const buttonHandler = () => { window.location.href = '/add'; };
 
   return (
     <div className="container">
@@ -43,15 +39,17 @@ export default function MainPage({ user, activity }) {
       <h4> Фильтрация по:</h4>
       <div style={{ display: 'flex' }}>
         <h5>уровню</h5>
-        <select className="form-select form-select-sm" name="level" onChange={changeHandler} value={searchLevel.level} aria-label=".form-select-sm example">
+        <select className="form-select form-select-sm" name="level" onChange={changeHandlerLevel} aria-label=".form-select-sm example">
+          <option value="">Выберите уровень</option>
           {fo.level?.map((el) => (
-            <option key={el.id}>{el.name}</option>
+            <option value={el.id} key={el.id}>{el.name}</option>
           ))}
         </select>
         <h5>разделу</h5>
-        <select className="form-select form-select-sm" name="razdel" aria-label=".form-select-sm example" value={searchLevel.razdel} onChange={changeHandler}>
+        <select className="form-select form-select-sm" name="razdel" aria-label=".form-select-sm example" onChange={changeHandlerCategory}>
+          <option value="">Выберите раздел</option>
           {fo.category?.map((el) => (
-            <option key={el.id}>{el.name}</option>
+            <option value={el.id} key={el.id}>{el.name}</option>
           ))}
         </select>
         <h5>статусу</h5>
@@ -62,7 +60,7 @@ export default function MainPage({ user, activity }) {
       </div>
       <h2 style={{ marginTop: '20px' }}>Городские инициативы</h2>
       <a href="/add" style={{ width: '300px', margin: '0 auto', display: 'flex' }} type="button" className="btn btn-primary">Добавить инициативу</a>
-      {init?.map((el) => (
+      {(isSearchLevel || isSearchCategory ? (initFilter.map((el) => (
         <div className="card-group">
           <div className="card">
             <div className="card-body">
@@ -73,9 +71,21 @@ export default function MainPage({ user, activity }) {
             </div>
           </div>
         </div>
-      ))}
+      )))
+        : (init?.map((el) => (
+          <div className="card-group">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">{el.title}</h5>
+                <h5 className="card-title">{el.term }</h5>
+                <h5 className="card-title">{el.Level.name}</h5>
+                <h5 className="card-title">{el.Category.name}</h5>
+              </div>
+            </div>
+          </div>
+        ))))}
 
-      <div>{cards?.map((init) => <Page key={init.id} activity={init} />)}</div>
+      {/* <div>{cards?.map((init) => <Page key={init.id} activity={init} />)}</div> */}
     </div>
 
   );
